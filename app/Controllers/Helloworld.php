aad0154a1c477894c5a13977dd6d19bc6d213630
evaniaintiha <evania.intiha@yahoo.com>
2020-11-16 20:43:23 +0700

    commitku2

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,18 @@
+<?php namespace App\Controllers;
+
+
+class Helloworld extends BaseController {
+
+	public function index($name, $npm)
+	{
+		echo $name ."<br";
+		echo $npm ."<br>";
+		//echo "Hello Vani";
+	}
+	public function show()
+	{
+		echo "Evania Intiha";
+		echo "1817051044";
+
+	}
+}
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,7 +10,7 @@ class Home extends BaseController
 	public function show()
 	{
 		$data['nama'] = 'Evania Intiha';
-		$data['jurusan'] = 'Ilmu Komputer';
+		$data['npm'] = '1817051044';
 		echo view('mahasiswa/header');
 		echo view('mahasiswa/index', $data);
 		echo view('mahasiswa/footer');
